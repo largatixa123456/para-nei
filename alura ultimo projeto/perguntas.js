315e127141a09a3170d0a9c05bbb24c882a65862
largatixa123456 <168498320+largatixa123456@users.noreply.github.com>
2024-11-25 21:29:14 -0300

    Add files via upload

---
+++ alura ultimo projeto/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'corinthians 2012',
+    'quem ganhou?',
+    'corinthians'
+)
+
+criaCartao(
+    'paulista',
+    'quem tem mais?',
+    'corinthians'
+)
+
+criaCartao(
+    'atlhetico',
+    'onde fica o h?',
+    'nao sei'
+)
+
+criaCartao(
+    'europa',
+    'quem conquistou a europa?',
+    'cristiano ronaldo'
+)
